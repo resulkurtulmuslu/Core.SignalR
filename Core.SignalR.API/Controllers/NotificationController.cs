b9bc5bd69e34c66d99dfac96c8b4d200b1d60da0
Resul Kurtulmuşlu <resulkurtulmuslu@outlook.com>
2021-04-19 01:40:34 +0300

    IHubContext<>

---
+++ Core.SignalR.API/Controllers/NotificationController.cs
@@ -0,0 +1,31 @@
+﻿using Core.SignalR.API.Hubs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core.SignalR.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationController : ControllerBase
+    {
+        private readonly IHubContext<MyHub> _hubContext;
+
+        public NotificationController(IHubContext<MyHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        [HttpGet("{teamCount}")]
+        public async Task<IActionResult> SetTeamCount(int teamCount)
+        {
+            await _hubContext.Clients.All.SendAsync("Notify", $"Arkadaşlar takım {teamCount} kişi olacaktır.");
+
+            return Ok();
+        }
+    }
+}
