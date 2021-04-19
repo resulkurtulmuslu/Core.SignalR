15bdb9d7330b781df4c4d03295828f2fbe41f1fc
Resul Kurtulmuşlu <resulkurtulmuslu@outlook.com>
2021-04-19 22:43:26 +0300

    Server Strongly typed Hubs

---
+++ Core.SignalR.API/Hubs/IProductHub.cs
@@ -0,0 +1,13 @@
+﻿using Core.SignalR.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core.SignalR.API.Hubs
+{
+    public interface IProductHub
+    {
+        Task ReceiveProduct(Product p);
+    }
+}
---
+++ Core.SignalR.API/Hubs/ProductHub.cs
@@ -0,0 +1,17 @@
+﻿using Core.SignalR.API.Models;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core.SignalR.API.Hubs
+{
+    public class ProductHub : Hub<IProductHub>
+    {
+        public async Task ReceiveProduct(Product p)
+        {
+            await Clients.All.ReceiveProduct(p);
+        }
+    }
+}
